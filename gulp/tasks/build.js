3306e62b8d3311d624c461df5a3062118aab0c6c
袴田 俊輔 <hakamata_shunsuke@cyberagent.co.jp>
2015-08-02 13:55:12 +0900

    add build task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var dest = './dist';
+
+gulp.task('build', function(done) {
+  return runSequence(
+    'build:js',
+    'build:css',
+    'build:html',
+    done
+  );
+});
+
+gulp.task('build:html', function() {
+  return gulp.src('./src/*.html')
+    .pipe(gulp.dest(dest));
+});
+
+gulp.task('build:js', function() {
+  return gulp.src('./src/js/**/*.js')
+    .pipe(gulp.dest(dest + '/js'));
+});
+
+gulp.task('build:css', function() {
+  return gulp.src('./src/css/**/*.css')
+    .pipe(gulp.dest(dest + '/css'));
+});
+
